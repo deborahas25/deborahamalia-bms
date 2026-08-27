b205eed086cc187763afbf0cd78394cd623b2273
Deborah <deborah@Deborahs-MacBook-Pro.local>
2026-08-27 17:16:40 +0700

    Tugas BMS

--- script.js
+++ script.js
@@ -3,11 +3,18 @@
 // === Quest 1 ===
 
 // 1. Ambil logo (class="logo")
-
+const logo = document.querySelector('.logo');
 // 2. Klik logo → alert (contoh: alert('Selamat datang!'))
-
+logo.addEventListener('click', function(){
+    alert('Selamat datang!');
+})
 // === Quest 2 ===
 
 // 3. Ambil semua kartu (.card)
-
+const cards = document.querySelectorAll('.card');
 // 4. Klik kartu → alert isi kartunya (hint: card.querySelector('p').textContent)
+cards.forEach(function(card){
+    card.addEventListener('click', function(){
+        alert(card.querySelector('p').textContent);
+    })
+})
